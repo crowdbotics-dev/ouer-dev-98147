4554b444c105c3b044ba68634b59a4a64b1cc507
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-06 06:44:10 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cbnf,Sghty
+admin.site.register(Cbnf)
+admin.site.register(Sghty)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+class Cbnf(models.Model):
+    'Generated Model'
+    dndjd = models.BigIntegerField()
+class Sghty(models.Model):
+    'Generated Model'
+    vncn = models.BigIntegerField()
+    cvddb = models.BigIntegerField()
+    ierueu = models.BigIntegerField()
+    rel_cbnf_1_1 = models.OneToOneField("home.Cbnf",blank=True,null=True,on_delete=models.CASCADE,related_name="sghty_rel_cbnf_1_1",)
